5b1ce876a4fd120ef00f5adc3d4ba7eb54ba066e
ljewalsh <ljewalsh@gmail.com>
2017-02-03 14:32:13 +1300

    Removed console.log and added more comments

--- Stuff You Stuff Comments.js
+++ Stuff You Stuff Comments.js
@@ -6,19 +6,19 @@
 // @namespace  http://www.stuff.co.nz
 // @include http://www.stuff.co.nz/*
 // ==/UserScript==
-function random_num_from_interval(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+function random_num_from_interval(min,max)
+                {
+                return Math.floor(Math.random() * (max - min) + min);
+                }
 
-var new_comments = ["I'm an idiot", "I have a stupid opinion", "Hurp durp", "Probably just a troll", "I was raised poorly", "Ignorance is bliss"];
+var new_comments = ["I'm an idiot","I have a stupid opinion","Hurp durp","Probably just a troll","I was raised poorly","Ignorance is bliss","Just another stuff commenter", "Something about PC-gone-mad", "Even my parents can't stand me"];
 
-window.onload = function () {
+window.onload = function(){
 
-  var old_comments = document.getElementsByClassName("gig-comments-comment-body");
-
-  for (i = 0; i < old_comments.length; i++) {
-    var comment = (new_comments[random_num_from_interval(0, new_comments.length)]);
-    old_comments[i].innerHTML = comment;
-    console.log(old_comments[i]);
-  }
+    var old_comments = document.getElementsByClassName("gig-comment-body");    
+    console.log(old_comments);
+    for (i=0;i<old_comments.length;i++){
+                var comment = (new_comments[random_num_from_interval(0,new_comments.length)]);               
+                old_comments[i].innerHTML = comment;
+                }
 };
